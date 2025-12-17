60e3b68b593980da6bd5b41726da3e81e69d8d19
SLargo18 <santiagolargo0118@gmail.com>
2025-12-17 16:53:54 +0100

    doing python module1

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
+    name = "Rose"
+    height = 25
+    age = 30
+    print(f"Plant: {name}", f"\nHeight: {height}cm", f"\nAge: {age} Days")
+    print("\n=== End of program ===")
