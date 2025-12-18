ca609942f4e2274651412f180ca024aa2b08b556
SLargo18 <santiagolargo0118@gmail.com>
2025-12-18 02:42:19 +0100

    ex1 done2

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,4 +1,4 @@
-class Plant:
+""" class Plant:
     def __init__(self, name, height, age):
         self.name = name
         self.height = height
@@ -11,4 +11,22 @@ cactus = Plant("Cactus", 15, 120)
 print("=== Garden Plant Registry ===")
 print(f"{rose.name}: {rose.height}cm, {rose.age} days old")
 print(f"{sunflower.name}: {sunflower.height}cm, {sunflower.age} days old")
-print(f"{cactus.name}: {cactus.height}cm, {cactus.age} days old")
+print(f"{cactus.name}: {cactus.height}cm, {cactus.age} days old") """
+
+class Plant:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show_info(self):
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+rose = Plant("Rose", 25, 30)
+sunflower = Plant("Sunflower", 80, 45)
+cactus = Plant("Cactus", 15, 120)
+
+print("=== Garden Plant Registry ===")
+rose.show_info()
+sunflower.show_info()
+cactus.show_info()
